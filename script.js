9aa85328599d68eebc21ecb4d7a7e17a2f7c8e39
Kye Moon <93773410+jessybous@users.noreply.github.com>
2025-02-05 09:14:54 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+let buttonno = document.querySelector(".no")
+
+let buttonyes = document.querySelector(".yes")
+
+let buttonno2 = document.querySelector(".two")
+
+let buttonno3 = document.querySelector(".three")
+
+let buttonyes4 = document.querySelector(".yesfour")
+
+let krill = document.querySelector(".krill")
+
+let cpr = document.querySelector(".cprstart")
+
+let cprfirst = document.querySelector(".cprfirst")
+
+let cprsecond = document.querySelector(".cprsecond")
+
+let cprthird = document.querySelector(".cprthird")
+
+let cprfin = document.querySelector(".cprfin")
+
+let restart = document.querySelector(".restart")
+
+
+if (buttonno  !== null) {
+    buttonno .addEventListener("click", function () {
+      window.location.href = "page2.html";
+    });
+  };
+
+  if (buttonyes  !== null) {
+    buttonyes .addEventListener("click", function () {
+      window.location.href = "theend.html";
+    });
+  };
+  if (buttonno2  !== null) {
+    buttonno2 .addEventListener("click", function () {
+      window.location.href = "page3.html";
+    });
+  };
+
+  
+  if (buttonno3  !== null) {
+    buttonno3 .addEventListener("click", function () {
+      window.location.href = "page4.html";
+    });
+  };
+
+    
+  if (buttonyes4  !== null) {
+    buttonyes4 .addEventListener("click", function () {
+      window.location.href = "pageyes4.html";
+    });
+  };
+
+  if (krill  !== null) {
+    krill .addEventListener("click", function () {
+      window.location.href = "page4.html";
+    });
+  };
+
+  if (cpr  !== null) {
+    cpr .addEventListener("click", function () {
+      window.location.href = "cpr.html";
+    });
+  };
+
+  if (cprfirst  !== null) {
+    cprfirst .addEventListener("click", function () {
+      window.location.href = "cpr2.html";
+    });
+  };
+
+  if (cprsecond  !== null) {
+    cprsecond .addEventListener("click", function () {
+      window.location.href = "cpr3.html";
+    });
+  };
+
+  if (cprthird  !== null) {
+    cprthird .addEventListener("click", function () {
+      window.location.href = "cpr4.html";
+    });
+  };
+
+  if (cprfin  !== null) {
+    cprfin .addEventListener("click", function () {
+      window.location.href = "theend.html";
+    });
+  };
+
+  if (restart  !== null) {
+    restart .addEventListener("click", function () {
+      window.location.href = "index.html";
+    });
+  };
